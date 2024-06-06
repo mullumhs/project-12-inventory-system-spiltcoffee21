a4055c48ab6ca528c9d72725904c66ed4f27d964
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2024-06-06 23:48:43 +0000

    regtdfg

--- lab2.py
+++ lab2.py
@@ -10,7 +10,7 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Step 1: Import the Item class from lab1.py
-from lab1 import item
+from lab1 import Item
 
 
 
@@ -19,47 +19,44 @@ from lab1 import item
 # It should include methods to add, remove, update, and display items in the inventory.
 class Inventory_Manager():
     def __init__(self,):
-        self.item = []
+        self.items = []
     
-    def add_item (self, name, price, quantity):
-        for existing_item in self.item:
-            if existing_item.get_name() == item.get_item():
+    def add_Item (self, name, price, quantity):
+        for existing_item in self.items:
+            if existing_item.get_name() == name:
                 print(f"{name}already added")               
-                return 
-        item = item(name, price, quantity) 
-        self.item.append(item)
-        print(f"item added: {name}")
-        return True
+                return False
+        item = Item(name, price, quantity) 
+        self.items.append(item)
+        print(f"Item added: {name}")
+        return True    
     
-    
-    
-    
-    def remove_item(self):
-        for i, existing_item in (self.item):
-            if existing_item.get_name() == item.get_item():
-                del self.item[i]
+    def remove_Item(self, name):
+        for existing_item in self.items:
+            if existing_item.get_name() == name:
+                self.items.remove(existing_item)
                 return True
         return False
     
-    
-    
-    def update_item(self, price):
-        for existing_item in self.item:
-            if existing_item.get_ticket_id() == item:
+    def update_price(self, name, price):
+        for existing_item in self.items:
+            if existing_item.get_name() == name:
                 existing_item.set_price(price)  # Update the price
                 return True
         return False
-                
-        
-        
-        
-    
-
-
 
 
 # Step 2: Create instances of the Item class and InventoryManager, then demonstrate their usage.
 # E.g. add items to the inventory, remove items, update items, and display the inventory.
 
+wepond_isle =Inventory_Manager()
 
+wepond_isle.add_Item("ak47", 307, 12)
 
+wepond_isle.add_Item("rpg", 12000, 34)
+
+wepond_isle.add_Item("Crucible blade", 100000000000, 2)
+
+wepond_isle.add_Item("Remy", 10, 3)
+
+wepond_isle.add_Item("drakes glizzy", 69, 13)
