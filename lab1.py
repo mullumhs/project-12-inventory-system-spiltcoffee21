8aa36e2fd6bd139019a9ca84bf72b2b0946ef997
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2024-06-11 04:59:53 +0000

    fthfgh

--- lab1.py
+++ lab1.py
@@ -54,10 +54,10 @@ class Item():
 # This method should check if the provided value is a non-negative number before setting the _price attribute.
 # If the value is negative, it should raise a ValueError.
     def set_price(self, price):
-        if price < 0:
-            raise ValueError("price cannot be negative")
-        self._price = price
-        
+        if isinstance(price,(int,float)) and price >= 0:
+            self._price = price
+        else:
+             raise ValueError("price cannot be negative")
 
 
 
@@ -75,9 +75,10 @@ class Item():
 # This method should check if the provided value is a non-negative integer before setting the _quantity attribute.
 # If the value is negative, it should raise a ValueError.
 def set_quantity(self, quantity):
-    if quantity < 0:
-        raise ValueError ("quantity cannot be negative")
-    self._quantity = quantity
+    if isinstance(quantity,(int,float)) and quantity >= 0:
+        self._quantity = quantity
+    else:
+        raise ValueError("wow")
 
 
 
@@ -85,10 +86,7 @@ def set_quantity(self, quantity):
 # Step 8: Create instances of the Item class and demonstrate the use of getters and setters.
 # For example, create a new Item and attempt to set its attributes with both valid and invalid values.
 # Print the outputs using the getters to show how the data is managed internally.
-fish = Item("fish", 5000000000000, 1)
-apple = Item("", 7, 43)
-paper = Item("paper", 600, -5)
-water = Item("water", -5, -30 )
+
 
 
 
